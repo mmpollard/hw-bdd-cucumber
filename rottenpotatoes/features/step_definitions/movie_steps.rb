a81ba264db4cc2bd97750df20f090dbbb27b684c
Max Pollard <mmpollard@berkeley.edu>
2016-02-21 00:52:33 +0000

    finished part2 and 3

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,7 +4,7 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-    Movie.create!(movie)
+    Movie.create(movie)
   end
 end
 
@@ -14,7 +14,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  regexp = /#{e1}.*{e2}/m #m makes .* match with n new lines
+  expect(page.body).to =~ regexp 
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,10 +26,18 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  ratings = rating_list.split(", ")
+  ratings.each do |r|
+    val = "ratings_#{r}"
+    if uncheck
+      uncheck val
+    else
+      check val
+    end
+  end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  expect(page).to have_css("table#movies tbody tr", count: Movie.count)
 end
